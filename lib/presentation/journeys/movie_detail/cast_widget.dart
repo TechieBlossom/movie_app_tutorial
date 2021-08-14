89aacfe9665d7b8a6c51285b5b4e9bcf016d972c
PrateekSharma1712 <prateeksharmacitm2011@gmail.com>
2021-08-14 14:56:50 +0530

    after pushing a initial build to internal track

--- lib/presentation/journeys/movie_detail/cast_widget.dart
+++ lib/presentation/journeys/movie_detail/cast_widget.dart
@@ -1,7 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:movieapp/presentation/blocs/theme/theme_cubit.dart';
+import '../../blocs/theme/theme_cubit.dart';
 
 import '../../../common/constants/size_constants.dart';
 import '../../../common/extensions/size_extensions.dart';
--- lib/presentation/movie_app.dart
+++ lib/presentation/movie_app.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:movieapp/presentation/blocs/theme/theme_cubit.dart';
+import 'blocs/theme/theme_cubit.dart';
 
 import '../common/constants/languages.dart';
 import '../common/constants/route_constants.dart';
